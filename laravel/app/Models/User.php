afa37d4906569d644449ec9b762861e027c7a779
Wall <ilnazamirhanov61@gmail.com>
2023-02-20 23:44:09 +0300

    add of documentation

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -27,6 +27,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'telegram_user_id',
     ];
 
     /**
@@ -58,4 +59,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+    public function routeNotificationForTelegram()
+    {
+        return $this->telegram_user_id;
+    }
 }
